6033d6fc50f2a9a184505fda3cd70244db6ab095
Arshnul <167389520+Arshillion@users.noreply.github.com>
2024-09-16 12:40:49 +0600

    Add files via upload

---
+++ carpetmart/best-seller.js
@@ -0,0 +1,315 @@
+const bellSeller = [
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+	{
+		link: '/sarah-sar-02-moss-area-rug-magnolia-home-by-joanna-gaines/',
+		title: 'Sarah SAR-02 Moss Area Rug - Magnolia Home by Joanna Gaines',
+		src: 'https://cdn11.bigcommerce.com/s-7rjd0xqdp0/images/stencil/270x360/products/19561/59532/SARASAR-02MO00__19422.1.jpg',
+	},
+];
+
+const getSingleSeller = (seller) => `
+<div class="single-product">
+    <img src=${seller.src}>
+    <strong>${seller.title}</strong>
+    <a class="info" href=${seller.link}>${seller.title}</a>
+    <a href=${seller.link}>VIEW</a>
+</div>
+`;
+
+const style = `
+<style>
+    #s-56dceeca-b58c-4d7a-88c4-4270f6bf464a{
+        display: none!important;
+    }
+    .new-seller-elm {
+        background: white;
+        margin: 30px 0;
+        padding: 30px 0;
+        position: relative;
+    }
+    .new-seller-elm::after {
+        content: '';
+        position: absolute;
+        background: #fff;
+        right: -100%;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        z-index: 999;
+    }
+    .new-seller-elm::before {
+        content: '';
+        position: absolute;
+        background: #fff;
+        left: -100%;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        z-index: 999;
+    }
+    .new-seller-elm *{
+        margin:0;
+        padding:0;
+    }
+    .new-seller-elm .header-best{
+        display: flex;
+        align-items: center;
+        gap:20px;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+    .new-seller-elm .header-best h1{
+        font-size: 32px;
+        font-weight: 900;
+        line-height: 38px;
+        text-align: left;
+    }
+    .new-seller-elm .header-best span{
+        border: 1px solid #FF0000;
+        flex-grow: 1;
+    }
+    .new-seller-elm .header-best .rotate-btns{
+        display: flex;
+    }
+    .new-seller-elm .header-best .rotate-btns button{
+        display: grid;
+        place-items: center;
+        border:1px solid #000000;
+        width:40px;
+        height:40px;
+    }
+    .new-seller-elm .header-best .rotate-btns button:last-child{
+        border-left: none;
+    }
+    .new-seller-elm .header-best .rotate-btns button svg{
+        width: 20px;
+        height: 20px;
+    }
+    .new-seller-elm .sellers-section{
+        position: relative;
+        width:100%;
+    }
+    .new-seller-elm .all-sellers{
+        display: grid;
+        grid-template-columns: repeat(15, 24%);
+        gap: 1.7%;
+        overflow-y: auto;
+        scroll-snap-type: x mandatory;
+		scrollbar-width: none;
+		padding: 10px 0;
+    }
+    .new-seller-elm .all-sellers::-webkit-scrollbar {
+		display: none;
+	}
+    .new-seller-elm .all-sellers .single-product{
+        scroll-snap-align: start;
+        display: grid;
+        gap: 20px;
+        padding:20px;
+        background: white;
+        border-radius: 6px;
+        box-shadow: 0px 2px 6px 0px #ccc;
+    }
+    .new-seller-elm .all-sellers .single-product img{
+        width: 100%;
+    }
+    .new-seller-elm .all-sellers .single-product strong{
+        font-size: 16px;
+        font-weight: 500;
+        text-align: center;
+        color: #000;
+    }
+    .new-seller-elm .all-sellers .single-product a.info{
+        font-size: 16px;
+        font-weight: 500;
+        color: #777;
+        text-decoration: underline;
+        text-align: center;
+    }
+    .new-seller-elm .all-sellers .single-product a:last-child{
+        color: #fff;
+        background-color: #627e36;
+        padding: 20px;
+        text-align: center;
+        font-weight: 700;
+        font-size: 20px;
+        display:none;
+    }
+    .new-seller-elm .all-sellers .single-product:hover a:last-child{
+        display:block;
+    }
+    @media only screen and (max-width:1200px){
+		.new-seller-elm .all-sellers {
+			grid-template-columns: repeat(15, 32%);
+			gap: 2%;
+		}
+	}
+	@media only screen and (max-width:991px){
+		.new-seller-elm .all-sellers {
+			grid-template-columns: repeat(15, 48%);
+			gap: 4%;
+		}
+	}
+	@media only screen and (max-width:480px){
+		.new-seller-elm .all-sellers {
+			grid-template-columns: repeat(15, 100%);
+			gap: 0;
+		}
+        .new-seller-elm .header-best h1{
+            font-size: 20px;
+            font-weight: 700;
+            line-height: 22px;
+        }
+	}
+</style>
+`;
+
+const new_elm_html = `
+<div class="new-seller-elm">
+	<div class="header-best">
+		<h1>BEST SELLERS</h1>
+		<span class="str-line"></span>
+		<div class="rotate-btns">
+			<button class="prev">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#000000"
+					width="800px"
+					height="800px"
+					viewBox="0 0 32 32"
+					version="1.1"
+				>
+					<path
+						d="M23.505 0c0.271 0 0.549 0.107 0.757 0.316 0.417 0.417 0.417 1.098 0 1.515l-14.258 14.264 14.050 14.050c0.417 0.417 0.417 1.098 0 1.515s-1.098 0.417-1.515 0l-14.807-14.807c-0.417-0.417-0.417-1.098 0-1.515l15.015-15.022c0.208-0.208 0.486-0.316 0.757-0.316z"
+					/>
+				</svg>
+			</button>
+			<button class="next">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#000000"
+					width="800px"
+					height="800px"
+					viewBox="0 0 32 32"
+					version="1.1"
+				>
+					<path
+						d="M8.489 31.975c-0.271 0-0.549-0.107-0.757-0.316-0.417-0.417-0.417-1.098 0-1.515l14.258-14.264-14.050-14.050c-0.417-0.417-0.417-1.098 0-1.515s1.098-0.417 1.515 0l14.807 14.807c0.417 0.417 0.417 1.098 0 1.515l-15.015 15.022c-0.208 0.208-0.486 0.316-0.757 0.316z"
+					/>
+				</svg>
+			</button>
+		</div>
+	</div>
+	<div class="sellers-section">
+		<div class="all-sellers">
+			${bellSeller.map((seller) => getSingleSeller(seller)).join('')}
+		</div>
+	</div>
+</div>
+`;
+
+const interval = setInterval(() => {
+	const exist_elm = document.querySelector(
+		'#s-59ded273-fd06-4e7c-9c05-ab0aa79fb6f2'
+	);
+	const head = document.querySelector('head');
+	if (exist_elm && head && !document.querySelector('.new-seller-elm')) {
+		head.insertAdjacentHTML('beforeend', style);
+		exist_elm.insertAdjacentHTML('afterend', new_elm_html);
+		clearInterval(interval);
+	}
+}, 10);
+
+const btn_interval = setInterval(() => {
+	const next_btn = document.querySelector(
+		'.new-seller-elm .header-best .rotate-btns .next'
+	);
+	const prev_btn = document.querySelector(
+		'.new-seller-elm .header-best .rotate-btns .prev'
+	);
+	const all_sell = document.querySelector('.sellers-section .all-sellers');
+	if (next_btn && prev_btn && all_sell) {
+		prev_btn.addEventListener('click', () => {
+			all_sell.scrollBy({
+				left: -20,
+				behavior: 'smooth',
+			});
+		});
+		next_btn.addEventListener('click', () => {
+			all_sell.scrollBy({
+				left: 20,
+				behavior: 'smooth',
+			});
+		});
+		clearInterval(btn_interval);
+	}
+}, 10);
